f5e79bd605fe4688596c11d09e36c11a97da9172
Gilles Castro <gillescastro1021@gmail.com>
2022-01-25 22:50:30 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const thankYouCards=[]
+function writeCards(giftGivers, giftType) {
+    for(let i = 0; i < giftGivers.length; i++) {
+      thankYouCards.push(`Thank you, ${giftGivers[i]}, for the wonderful ${giftType} gift!`);
+      debugger;
+    }
+    return thankYouCards;
+    }
+  function countDown(number){
+  while(number >= 0) {
+  console.log(number--)
+  debugger;
+  }
+  return number;
+  }
+  
